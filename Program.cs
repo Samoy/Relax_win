eb54800627628608fb24cdc8431e58820130ee56
Samoy <samoy_young@163.com>
2021-02-27 18:13:12 +0800

    添加提醒功能

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Relax.Properties;
 using System;
+using System.Timers;
 using System.Windows.Forms;
 
 public class MainForm : Form
@@ -10,19 +11,25 @@ public class MainForm : Form
     private readonly string[] subMenus = { "30分钟", "1小时", "2小时" };
     private bool isRemain = true; // 默认打开提醒
     private int remainIndex = 1; // 默认提醒时间为1小时
+    private readonly System.Timers.Timer timer = new System.Timers.Timer();
 
     public NotifyIcon NotifyIcon { get; set; }
 
     [STAThread]
     static void Main()
     {
-        Application.Run(new MainForm());
+        Application.EnableVisualStyles(); 
+        Application.SetCompatibleTextRenderingDefault(false);          
+        Application.Run(new MainForm());   
     }
 
     public MainForm()
     {
         components = new System.ComponentModel.Container();
         contextMenu = new ContextMenu();
+        Visible = false;
+        WindowState = FormWindowState.Minimized;
+        ShowInTaskbar = false;
         InitNotifyIcon();
         InitMenus();
     }
@@ -35,6 +42,35 @@ public class MainForm : Form
         };
         NotifyIcon.ContextMenu = contextMenu;
         NotifyIcon.Visible = true;
+        NotifyIcon.BalloonTipText = $"你已经工作了{subMenus[remainIndex]}了，休息一会儿吧！O(∩_∩)O";
+        LoopRun();
+    }
+
+    private void LoopRun()
+    {
+        timer.Stop();
+        timer.Interval = 1000 * getSeconds(remainIndex);
+        timer.AutoReset = true;
+        timer.Enabled = true;
+        timer.Elapsed += new ElapsedEventHandler(ShowNotify);
+    }
+
+    private void ShowNotify(Object sender, ElapsedEventArgs args)
+    {
+        NotifyIcon.ShowBalloonTip(1000);
+    }
+
+    private int getSeconds(int remainIndex)
+    {
+        if (remainIndex == 1)
+        {
+            return 60 * 60;
+        }
+        if (remainIndex == 2)
+        {
+            return 2 * 60 * 60;
+        }
+        return 30 * 60;
     }
 
     private void InitMenus()
@@ -73,9 +109,10 @@ public class MainForm : Form
     {
         MenuItem subMenuItem = (MenuItem)sender;
         remainIndex = Array.IndexOf(subMenus, subMenuItem.Text);
+        LoopRun();
         foreach (MenuItem item in contextMenu.MenuItems[1].MenuItems)
         {
-            item.Checked = Array.IndexOf(subMenus,item.Text) == remainIndex;
+            item.Checked = Array.IndexOf(subMenus, item.Text) == remainIndex;
         }
     }
 
@@ -86,6 +123,19 @@ public class MainForm : Form
         {
             isRemain = !isRemain;
             item.Checked = isRemain;
+            if (isRemain)
+            {
+                LoopRun();
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+        if ("退出".Equals(item.Text))
+        {
+            timer.Dispose();
+            Application.Exit();
         }
     }
 }
